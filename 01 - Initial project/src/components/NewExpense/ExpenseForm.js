724959113ec972fa57fbf52566bd2996f24db6b7
zBiggui <bigguisenna@gmail.com>
2021-12-25 18:54:25 -0300

    Component to handle with forms (Module 2)

--- 01 - Initial project/src/components/NewExpense/ExpenseForm.js
+++ 01 - Initial project/src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,100 @@
+import React, { useState } from 'react';
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // })
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value)
+
+        // Get all previous pair of key-value and overwrite the desired one
+        // Not the best approach
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value
+        // })
+
+        // That way it guarantees that react will get the previous values immediately, otherwise it schedules to the call stack
+
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredTitle: event.target.value }
+        // })
+    }
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value)
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value
+        // })
+    }
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value
+        // })
+    }
+
+    const submitHander = (event) => {
+        // Prevent of page reload after submit
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+
+        // Reset values after submitting them
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHander}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text"></input>
+                    <input
+                        type="text"
+                        // two-way binding
+                        // Listen to changes and passes back to reset/change the values dynamically
+                        value={enteredTitle}
+                        onChange={titleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01"></input>
+                    <input
+                        type="number"
+                        min="0.01"
+                        step="0.01"
+                        value={enteredAmount}
+                        onChange={amountChangeHandler}
+                    />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31"></input>
+                    <input
+                        type="date"
+                        min="2019-01-01"
+                        max="2022-12-31"
+                        value={enteredDate}
+                        onChange={dateChangeHandler}
+                    />
                 </div>
             </div>
             <div className="new-expense__actions">
