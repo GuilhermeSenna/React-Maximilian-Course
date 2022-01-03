cce802631d31f0ab3a3ffcb911d4815175c3feff
zBiggui <bigguisenna@gmail.com>
2022-01-03 18:41:39 -0300

    UseContext to login/logout management

--- 06 - Login page (Advanced Handling Side Effects)/src/components/Login/Login.js
+++ 06 - Login page (Advanced Handling Side Effects)/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -53,6 +54,8 @@ const Login = (props) => {
     value: '',
     isValid: null,
   });
+
+  const authCtx = useContext(AuthContext)
 
   // This is used to the trigger useEffect only when the validity of this inputs is changed
   const { isValid: emailIsValid } = emailState;
@@ -107,7 +110,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
--- 06 - Login page (Advanced Handling Side Effects)/src/store/auth-context.js
+++ 06 - Login page (Advanced Handling Side Effects)/src/store/auth-context.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: () => { },
+    onLogin: (email, password) => { }
 });
+
+export const AuthContextProvider = props => {
+    const [isLoggedin, setIsLoggedIn] = useState(false);
+
+    // useEffect
+    // First Argument: Function
+    // Second Argument: Dependencies
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+        // Keep logged in after refreshing the page
+        if (storedUserLoggedInInformation === '1') {
+            setIsLoggedIn(true);
+        }
+
+    }, [])
+
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn')
+        setIsLoggedIn(false);
+    }
+
+    const loginHandler = () => {
+        localStorage.setItem('isLoggedIn', '1')
+        setIsLoggedIn(true);
+    }
+
+    // Return
+    return <AuthContext.Provider value={{
+        isLoggedIn: isLoggedin,
+        onLogout: logoutHandler,
+        onLogin: loginHandler
+    }}
+    >{props.children}</AuthContext.Provider>
+}
 
 export default AuthContext;
