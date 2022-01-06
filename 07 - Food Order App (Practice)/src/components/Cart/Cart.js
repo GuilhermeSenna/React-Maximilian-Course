450db43d2429af03e02f52f566ca476e7241802c
zBiggui <bigguisenna@gmail.com>
2022-01-06 20:21:17 -0300

    Incress Item quantity in cart and Small fix in amount amount value

--- 07 - Food Order App (Practice)/src/components/Cart/Cart.js
+++ 07 - Food Order App (Practice)/src/components/Cart/Cart.js
@@ -1,19 +1,36 @@
+import { useContext } from 'react';
+
 import Modal from '../UI/Modal';
 import styles from './Cart.module.css'
 import CartContext from '../../store/cart-context';
-import { useContext } from 'react';
+import CartItem from './CartItem';
 
 const Cart = props => {
 
     const cartCtx = useContext(CartContext);
 
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+    console.log(cartCtx)
+
     const hasItems = cartCtx.items.length > 0;
+
+    const cartItemRemoveHandler = id => { };
+
+    const cartItemAddHandler = item => {
+        cartCtx.addItem({ ...item, amount: 1 });
+    };
 
     const cartItems = (
         <ul className={styles['cart-items']}>
             {cartCtx.items.map((item) => (
-                <li key={Math.random().toString()}>{item.name}</li>
+                <CartItem
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount} price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)}
+                />
             ))}
         </ ul>
     );
@@ -23,7 +40,7 @@ const Cart = props => {
             {cartItems}
             <div className={styles.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={styles.actions}>
                 <button className={styles['button--alt']} onClick={props.onHideCart}>Close</button>
