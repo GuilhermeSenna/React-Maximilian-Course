4ab7a56182835abae730f291b163b3e6c4cb9325
zBiggui <bigguisenna@gmail.com>
2022-01-06 21:04:16 -0300

    Remove item logic added

--- 07 - Food Order App (Practice)/src/components/Cart/Cart.js
+++ 07 - Food Order App (Practice)/src/components/Cart/Cart.js
@@ -11,11 +11,11 @@ const Cart = props => {
 
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
 
-    console.log(cartCtx)
-
     const hasItems = cartCtx.items.length > 0;
 
-    const cartItemRemoveHandler = id => { };
+    const cartItemRemoveHandler = id => {
+        cartCtx.removeItem(id);
+    };
 
     const cartItemAddHandler = item => {
         cartCtx.addItem({ ...item, amount: 1 });
