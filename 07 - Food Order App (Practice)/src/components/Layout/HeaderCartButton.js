6d3d75138e617ddf804a4f2aa641076d8d832c84
zBiggui <bigguisenna@gmail.com>
2022-01-06 21:15:10 -0300

    Bump Effect in cart item

--- 07 - Food Order App (Practice)/src/components/Layout/HeaderCartButton.js
+++ 07 - Food Order App (Practice)/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,39 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import cartContext from '../../store/cart-context'
 import styles from './HeaderCartButton.module.css'
 
 const HeaderCartButton = props => {
+    const [btnIsHighLighted, setBtnIsHighLighted] = useState(false)
+
     const cartCtx = useContext(cartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    // Object destructuring
+    const { items } = cartCtx;
+
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
 
+    const btnStyles = `${styles.button} ${btnIsHighLighted ? styles.bump : ''} `;
+
+    useEffect(() => {
+        if (cartCtx.items.length === 0) return;
+
+        setBtnIsHighLighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighLighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
-        <button className={styles.button} onClick={props.onClick}>
+        <button className={btnStyles} onClick={props.onClick}>
             <span className={styles.icon}>
                 <CartIcon />
             </span>
