6cfdc6b197b76217379906931049013f851f3856
zBiggui <bigguisenna@gmail.com>
2022-01-13 20:11:11 -0300

    useCallback and image upload

--- 08 -/src/App.js
+++ 08 -/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Button from './components/UI/Button/Button'
 import './App.css';
 import DemoOutput from './components/Demo/DemoOutput';
@@ -6,18 +6,28 @@ import DemoOutput from './components/Demo/DemoOutput';
 function App() {
 
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('APP RUNNING')
 
-  const toggleParagraphHandler = () => {
-    setShowParagraph(prevShowParagraph => !prevShowParagraph);
+  // useCallback usage <- Function
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      // If depends of a previous snapshot of that state
+      setShowParagraph(prevShowParagraph => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
   }
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
-
+      {/* useDemo is used inside this component */}
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow toggling</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
--- 08 -/src/components/Demo/DemoOutput.js
+++ 08 -/src/components/Demo/DemoOutput.js
@@ -1,6 +1,9 @@
+import React from 'react';
+
 const DemoOutput = props => {
     console.log('DemoOutput Running')
     return <p>{props.show ? 'This is new!' : ''}</p>;
 }
 
-export default DemoOutput;
+// Only re-evaluate if has a change in the Component
+export default React.memo(DemoOutput);
