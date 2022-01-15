a059ac1fdbac432ad5e6719865da8961fb29f212
zBiggui <bigguisenna@gmail.com>
2022-01-15 17:29:48 -0300

    Props and state using class based components

--- 10 - Class-based components/src/components/User.js
+++ 10 - Class-based components/src/components/User.js
@@ -1,7 +1,16 @@
+import { Component, useState } from 'react';
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+// Changed to class
+class User extends Component {
+  render() {
+    // Added the this statement
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
