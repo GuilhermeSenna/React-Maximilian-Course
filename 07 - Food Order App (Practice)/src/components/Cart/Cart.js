8d7cc30a801642cd8314cda213d2cb9bdde7f8ee
zBiggui <bigguisenna@gmail.com>
2022-01-17 14:21:17 -0300

    Fixing project title grammar

--- 07 - Food Order App (Practice)/src/components/Cart/Cart.js
+++ 07 - Food Order App (Practice)/src/components/Cart/Cart.js
@@ -22,17 +22,20 @@ const Cart = props => {
     };
 
     const cartItems = (
-        <ul className={styles['cart-items']}>
-            {cartCtx.items.map((item) => (
-                <CartItem
-                    key={item.id}
-                    name={item.name}
-                    amount={item.amount} price={item.price}
-                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
-                    onAdd={cartItemAddHandler.bind(null, item)}
-                />
-            ))}
-        </ ul>
+        <>
+            <h1>teste</h1>
+            <ul className={styles['cart-items']}>
+                {cartCtx.items.map((item) => (
+                    <CartItem
+                        key={item.id}
+                        name={item.name}
+                        amount={item.amount} price={item.price}
+                        onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                        onAdd={cartItemAddHandler.bind(null, item)}
+                    />
+                ))}
+            </ ul>
+        </>
     );
 
     return (
